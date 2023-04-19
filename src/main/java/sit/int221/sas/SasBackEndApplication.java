9aa54eb0f4f9c9d33971a71e67b9e18cb92d69ad
Kasidatep <bank@kasidate.me>
2023-04-19 14:30:36 +0700

    Initial commit

---
+++ src/main/java/sit/int221/sas/SasBackEndApplication.java
@@ -0,0 +1,13 @@
+package sit.int221.sas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SasBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sit/int221/sas/SasBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.int221.sas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
