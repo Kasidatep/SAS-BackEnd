9e8477b055d0bc3fde95265c51b419099c097000
Kasidatep <bank@kasidate.me>
2023-04-26 18:36:12 +0700

    [UPDATE] CrossOrigin

--- src/main/java/sit/int221/sas/controllers/AnnouncementControllers.java
+++ src/main/java/sit/int221/sas/controllers/AnnouncementControllers.java
@@ -1,16 +1,14 @@
 package sit.int221.sas.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sit.int221.sas.dto.AllAnnouncement;
 import sit.int221.sas.dto.AnnouncementDetail;
 import sit.int221.sas.services.AnnouncementService;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/announcements")
 public class AnnouncementControllers {
